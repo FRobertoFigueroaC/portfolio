953a12baae004029e3747d77d3c57975a6c48b2c
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-08-25 16:09:51 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { Main, Resources, Portfolio, Education, Experience, Projects, Brands, Footer} from './components';
+
+
+function App() {
+
+  return (
+    <>
+      <Main/>
+      <Resources />
+      <Portfolio />
+      <Experience />
+      <Projects />
+      <Education />
+      <Brands />
+      <Footer />
+    </>
+  )
+}
+
+export default App
